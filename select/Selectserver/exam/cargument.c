fbbc5ee2f0f97f4ec80292591e2c38edd2e702bf
AlexFrMir <franc_@live.com.mx>
2022-03-23 09:32:24 -0600

    added exam

---
+++ select/Selectserver/exam/cargument.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("El numero de argumentos es: %d\n", argc);
+	printf("argv[1]: %s", argv[1]);
+	return 0;
+}
